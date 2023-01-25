e76112b4f75e2ef3d5c7fcbc84aa29f9555361e7
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2023-01-25 10:51:53 -0800

    add scss config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,7 +5,7 @@ import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
-import postcss from 'rollup-plugin-postcss';
+import scss from 'rollup-plugin-scss';
 import del from 'rollup-plugin-delete';
 
 import packageJson from './package.json' assert { type: 'json' };
@@ -29,7 +29,8 @@ export default {
     resolve(),
     commonjs(),
     typescript({ useTsconfigDeclarationDir: true }),
-    postcss(),
+    scss(),
+
     del({ targets: 'build/*' }),
   ],
 };
